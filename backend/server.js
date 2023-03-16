15f06f9604477a96f5434455ef7745245bce872b
Daniel Badura <daniel.badura@outlook.com>
2023-03-16 23:29:59 +0100

    Fetch Products from Backend

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ app.get('/api/products', (req, res) => {
     res.json(products);
 });
 app.get('/api/products/:id', (req, res) => {
-    const product = products.find(p => p._id === req.params.id);
+    const product = products.find((p) => p._id === req.params.id);
     res.json(product);
 });
 
