76331f8559f6007ddeec033a2ce35615f4a303a3
Daniel Badura <daniel.badura@outlook.com>
2023-03-18 13:26:28 +0100

    created db models

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,7 +5,7 @@ const connectDB = async () => {
     try {
         const connect = await mongoose.connect(process.env.MONGO_URI, {
         });
-        console.log(`MongoDB Connected: ${connect.connection.host}`.cyan.underline);
+        console.log(`MongoDB Connected: ${connect.connection.host}`.blue.underline);
     }
     catch (error) {
         console.error(`Error: ${error.message}`);
---
+++ backend/models/userModel.js
@@ -0,0 +1,38 @@
+import mongoose from 'mongoose';
+
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    lastname: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    phone: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    isAdmin: {
+        type: String,
+        required: true,
+        default: false
+    }
+},
+    {
+        timestamps: true
+    }
+);
+
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
