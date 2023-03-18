16990428ea43b7f816896e5591eacb8e48311f53
Daniel Badura <daniel.badura@outlook.com>
2023-03-18 20:14:38 +0100

    updated data feed

--- backend/data/products.js
+++ backend/data/products.js
@@ -1,22 +1,22 @@
 const products = [
   {
-    name: 'Airpods Wireless Bluetooth Headphones',
-    image: '/images/airpods.jpg',
+    name: 'Neuronet Implant',
+    image: '/images/neuronet.jpg',
     description:
-      'Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working',
-    brand: 'Apple',
-    category: 'Electronics',
+      'Device that can be surgically implanted into the brain to enhance cognitive abilities, allowing users to process information and learn at an accelerated rate',
+    brand: 'NexGen Innovations',
+    category: 'Implants',
     price: 89.99,
     countInStock: 0,
     rating: 4.5,
     numReviews: 12,
   },
   {
-    name: 'iPhone 11 Pro 256GB Memory',
-    image: '/images/phone.jpg',
+    name: 'Chrono-Distortion Field Generator',
+    image: '/images/cdfg.jpg',
     description:
-      'Introducing the iPhone 11 Pro. A transformative triple-camera system that adds tons of capability without complexity. An unprecedented leap in battery life',
-    brand: 'Apple',
+      'Device that creates a field of distorted time, allowing users to slow down or speed up time within a specific area',
+    brand: 'Quantum Dynamics',
     category: 'Electronics',
     price: 599.99,
     countInStock: 7,
@@ -24,11 +24,11 @@ const products = [
     numReviews: 8,
   },
   {
-    name: 'Cannon EOS 80D DSLR Camera',
-    image: '/images/camera.jpg',
+    name: 'Molecular Disassembler',
+    image: '/images/molecular-disassembler.jpg',
     description:
-      'Characterized by versatile imaging specs, the Canon EOS 80D further clarifies itself using a pair of robust focusing systems and an intuitive design',
-    brand: 'Cannon',
+      'handheld device that can break down any matter into its constituent atoms, allowing users to disintegrate objects or materials.',
+    brand: 'Synthetix Solutions',
     category: 'Electronics',
     price: 929.99,
     countInStock: 5,
@@ -36,11 +36,11 @@ const products = [
     numReviews: 12,
   },
   {
-    name: 'Sony Playstation 4 Pro White Version',
-    image: '/images/playstation.jpg',
+    name: 'Holo-Transmitter',
+    image: '/images/holo-transmitter.jpg',
     description:
-      'The ultimate home entertainment center starts with PlayStation. Whether you are into gaming, HD movies, television, music',
-    brand: 'Sony',
+      'Communication device that creates a holographic projection of the user, allowing them to appear in multiple locations at once',
+    brand: 'AstroGen',
     category: 'Electronics',
     price: 399.99,
     countInStock: 11,
@@ -48,10 +48,10 @@ const products = [
     numReviews: 12,
   },
   {
-    name: 'Logitech G-Series Gaming Mouse',
-    image: '/images/mouse.jpg',
+    name: 'Gravimetric Inverter:',
+    image: '/images/gravimetric-inverter.jpg',
     description:
-      'Get a better handle on your games with this Logitech LIGHTSYNC gaming mouse. The six programmable buttons allow customization for a smooth playing experience',
+      'Device that can manipulate gravitational forces, allowing users to create artificial gravity fields or even reverse the effects of gravity altogether',
     brand: 'Logitech',
     category: 'Electronics',
     price: 49.99,
@@ -60,17 +60,77 @@ const products = [
     numReviews: 10,
   },
   {
-    name: 'Amazon Echo Dot 3rd Generation',
-    image: '/images/alexa.jpg',
+    name: 'Gravity Boots',
+    image: '/images/gravity-boots.jpg',
     description:
-      'Meet Echo Dot - Our most popular smart speaker with a fabric design. It is our most compact smart speaker that fits perfectly into small space',
-    brand: 'Amazon',
-    category: 'Electronics',
+      'Boots that generate a magnetic field, allowing the wearer to walk on walls and ceilings.',
+    brand: 'AstroGen',
+    category: 'Boots',
     price: 29.99,
     countInStock: 0,
     rating: 4,
     numReviews: 12,
   },
+  {
+    name: 'Time-Freezer Watch',
+    image: '/images/tfw.jpg',
+    description:
+      'Wristwatch that can temporarily freeze time for a few seconds, allowing the wearer to react quickly to dangerous situations.',
+    brand: 'NexGen Innovations',
+    category: 'Electronics',
+    price: 89.99,
+    countInStock: 0,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    name: 'Invisibility Cloak',
+    image: '/images/invisibility-cloak.jpg',
+    description:
+      'Cloak made of a special material that bends light around the wearer, making them invisible.',
+    brand: 'NexGen Innovations',
+    category: 'Clothing',
+    price: 599.99,
+    countInStock: 7,
+    rating: 4.0,
+    numReviews: 8,
+  },
+  {
+    name: 'Mind-Reader Glasses',
+    image: '/images/mrg.jpg',
+    description:
+      'Glasses that can scan the brainwaves of those around the wearer, allowing them to "read" the thoughts and emotions of others.',
+    brand: 'Synthetix Solutions',
+    category: 'Misc',
+    price: 929.99,
+    countInStock: 5,
+    rating: 3,
+    numReviews: 12,
+  },
+  {
+    name: 'Hyper-Translation Earbuds',
+    image: '/images/htext-earbuds.jpg',
+    description:
+      'earbuds that instantly translate any language into the wearer\'s native tongue, allowing them to understand and communicate with anyone regardless of language barriers.',
+    brand: 'Quantum Dynamics',
+    category: 'Electronics',
+    price: 399.99,
+    countInStock: 11,
+    rating: 5,
+    numReviews: 12,
+  },
+  {
+    name: 'Gravimetric Inverter:',
+    image: '/images/gravity-inverter.jpg',
+    description:
+      'Device that can manipulate gravitational forces, allowing users to create artificial gravity fields or even reverse the effects of gravity altogether',
+    brand: 'Logitech',
+    category: 'Electronics',
+    price: 49.99,
+    countInStock: 7,
+    rating: 3.5,
+    numReviews: 10,
+  }
 ];
 
 export default products;
