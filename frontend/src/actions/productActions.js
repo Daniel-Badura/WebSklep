db5a2de8a2539780ad2d57251b396152735c1482
Daniel Badura <daniel.badura@outlook.com>
2023-03-19 18:29:42 +0100

    added Loader and Message component

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -21,8 +21,25 @@ export const listProducts = () => async (dispatch) => {
                 ? error.response.data.message
                 : error.message,
         });
-
-
     }
-
 };
+// export const singleProduct = () => async (dispatch) => {
+
+//     try {
+//         dispatch({ type: PRODUCT_LIST_REQUEST });
+//         const { data } = await axios.get(`/api/products/${}`);
+//         dispatch({
+//             type: PRODUCT_LIST_SUCCESS,
+//             payload: data
+//         });
+//     } catch (error) {
+//         dispatch({
+//             type: PRODUCT_LIST_FAIL,
+//             payload: error.response && error.response.data.message
+//                 ? error.response.data.message
+//                 : error.message,
+//         });
+
+
+//     }
+// };
---
+++ frontend/src/components/Loader.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Spinner } from 'react-bootstrap';
+
+const Loader = () => {
+    return (
+        <Spinner animation='border' role='status' style={{
+            width: '10rem',
+            height: '10rem',
+            margin: 'auto',
+            display: 'block',
+        }}
+        >
+            <span class='sr-only'> Loading ... </span>
+        </Spinner>
+    );
+};
+
+export default Loader;
