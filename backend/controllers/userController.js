9f27f84954e807a76043056509c133127ce3f4f2
Daniel Badura <daniel.badura@outlook.com>
2023-03-26 23:07:13 +0200

    added authentication middleware with user login route

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,6 @@
 import asyncHandler from "express-async-handler";
 import User from '../models/userModel.js';
+import token from "../utils/token.js";
 
 // @desc        Authenticate user and get the token
 // @route       POST /api/users/login
@@ -14,10 +15,31 @@ export const authUser = asyncHandler(async (req, res) => {
             name: user.name,
             email: user.email,
             isAdmin: user.isAdmin,
-            token: null
+            token: token(user._id),
         });
     } else {
         res.status(401);
         throw new Error('Invalid username or password' + " username " + await User.findOne({ email }));
     }
+});
+
+
+// @desc        Get user profile
+// @route       GET /api/users/profile
+// @access      Private
+export const getUserProfile = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.user._id);
+
+    if (user) {
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+        });
+    } else {
+        res.status(404);
+        throw new Error('User not found');
+    }
+    res.send("good");
 });
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,12 @@
 import express from "express";
 
 const router = express.Router();
-import { authUser } from "../controllers/userController.js";
+import { authUser, getUserProfile } from "../controllers/userController.js";
+import { authenticator } from "../middleware/authMiddleware.js";
+
 
 router.post('/login', authUser);
+router.route('/profile').get(authenticator, getUserProfile);
 
 
 export default router;
