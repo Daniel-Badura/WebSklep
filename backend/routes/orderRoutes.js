43c83cbaac1490e885c140694fd675567fa3a42a
Daniel Badura <daniel.badura@outlook.com>
2023-04-12 23:32:55 +0200

    added get order by id route

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,12 +1,13 @@
 import express from "express";
 
 const router = express.Router();
-import { addOrderItems } from "../controllers/orderController.js";
+import { addOrderItems, getOrderById } from "../controllers/orderController.js";
 import { authenticator } from "../middleware/authMiddleware.js";
 
 
 // Default route
 router.route('/').post(authenticator, addOrderItems);
+router.route('/:id').get(authenticator, getOrderById);
 // user ogin route
 
 
