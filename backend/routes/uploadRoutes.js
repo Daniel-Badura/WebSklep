e8c967139865ba48ae0666952819518c07fbc381
Daniel Badura <daniel.badura@outlook.com>
2023-05-20 04:21:04 +0200

    added Google Cloud Storage for the image save and display functionality. terrific experience

--- backend/routes/uploadRoutes.js
+++ backend/routes/uploadRoutes.js
@@ -1,55 +1,84 @@
-import express from 'express';
-import multer from 'multer';
-import path from 'path';
-import { authenticator, isAdmin } from '../middleware/authMiddleware.js';
-import File from '../models/fileModel.js';
+import express from "express";
+import multer from "multer";
+import path from "path";
+import colors from "colors";
+import { authenticator, isAdmin } from "../middleware/authMiddleware.js";
+import File from "../models/fileModel.js";
+import dotenv from "dotenv";
+import { Storage } from "@google-cloud/storage";
+
+dotenv.config();
 
 const router = express.Router();
 
-const storage = multer.diskStorage({
-    destination(req, file, cb) {
-        cb(null, 'uploads/');
-    },
-    filename(req, file, cb) {
-        cb(
-            null,
-            `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`
-        );
-    },
-});
-
 function checkFileType(file, cb) {
-    const filetypes = /jpg|jpeg|png/;
-    const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
-    const mimetype = filetypes.test(file.mimetype);
+  const filetypes = /jpg|jpeg|png/;
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+  const mimetype = filetypes.test(file.mimetype);
 
-    if (extname && mimetype) {
-        return cb(null, true);
-    } else {
-        cb('Images only!');
-    }
+  if (extname && mimetype) {
+    return cb(null, true);
+  } else {
+    cb("Images only!");
+  }
 }
 
+// @desc Google Cloud Storage
+let projectId = process.env.GOOGLE_CLOUD_PROJECT_ID;
+let credentials = JSON.parse(process.env.GOOGLE_CREDENTIALS);
+let bucketURL = process.env.GOOGLE_CLOUD_STORAGE_BUCKET_URL;
+let bucketName = process.env.GOOGLE_CLOUD_STORAGE_BUCKET_NAME;
+const storage = new Storage({
+  name: "websklep",
+  projectId: projectId,
+  credentials: credentials,
+});
+const bucket = storage.bucket(bucketName);
+
+// @desc Multer
 const upload = multer({
-    storage,
-    fileFilter: function (req, file, cb) {
-        checkFileType(file, cb);
-    },
+  storage: multer.memoryStorage(),
+  fileFilter: function (req, file, cb) {
+    checkFileType(file, cb);
+  },
+  limits: {
+    fileSize: 5 * 1024 * 1024,
+  },
 });
 
-router.post('/', authenticator, isAdmin, upload.single('image'), async (req, res) => {
+// @desc    Upload file
+router.post(
+  "/",
+  authenticator,
+  isAdmin,
+  upload.single("image"),
+  async (req, res) => {
     // res.send(`/${req.file.path}`);
     try {
+      if (req.file) {
+        let filename = req.file.originalname;
+        let path = `${bucketURL}/${filename}`;
         const file = new File({
-            filename: req.file.filename,
-            path: req.file.path,
+          filename: filename,
+          path: path,
         });
+
         await file.save();
-        res.status(201).json({ message: 'File uploaded successfully', path: path });
+        const blob = bucket.file(filename);
+        const blobStream = blob.createWriteStream();
+        blobStream.on("finish", () => {
+          res.status(200).json({
+            message: "File uploaded successfully",
+            filename: path,
+          });
+        });
+        blobStream.end(req.file.buffer);
+      }
     } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: 'Server error' });
+      console.error(error);
+      res.status(500).json({ message: "Server error" });
     }
-});
+  }
+);
 
 export default router;
--- frontend/src/screens/EditProductScreen.js
+++ frontend/src/screens/EditProductScreen.js
@@ -1,205 +1,215 @@
-import React, { useState, useEffect, } from 'react';
-import { Link, useNavigate, useParams } from 'react-router-dom';
-import { Form, Button } from 'react-bootstrap';
-import { useDispatch, useSelector } from 'react-redux';
-import Message from '../components/Message';
-import Loader from '../components/Loader';
-import FormContainer from '../components/FormContainer';
-import { PRODUCT_UPDATE_RESET } from '../constants/productConstats';
-import { listProductDetails, updateProduct } from '../actions/productActions';
-import axios from 'axios';
-import { useTranslation } from 'react-i18next';
-
-
+import React, { useState, useEffect } from "react";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import { Form, Button } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import Message from "../components/Message";
+import Loader from "../components/Loader";
+import FormContainer from "../components/FormContainer";
+import { PRODUCT_UPDATE_RESET } from "../constants/productConstats";
+import { listProductDetails, updateProduct } from "../actions/productActions";
+import axios from "axios";
+import { useTranslation } from "react-i18next";
 
 const EditProductScreen = () => {
-    const { t } = useTranslation();
-    const navigate = useNavigate();
-    const dispatch = useDispatch();
-    const { id: productId } = useParams();
-    const [name, setName] = useState('');
-    const [price, setPrice] = useState(0);
-    const [image, setImage] = useState('');
-    const [brand, setBrand] = useState('');
-    const [description, setDescription] = useState('');
-    const [category, setCategory] = useState('');
-    const [countInStock, setCountInStock] = useState('');
-    const [uploading, setUploading] = useState('');
-    const [featured, setFeatured] = useState('');
+  const { t } = useTranslation();
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { id: productId } = useParams();
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState(0);
+  const [image, setImage] = useState("");
+  const [brand, setBrand] = useState("");
+  const [description, setDescription] = useState("");
+  const [category, setCategory] = useState("");
+  const [countInStock, setCountInStock] = useState("");
+  const [uploading, setUploading] = useState("");
+  const [featured, setFeatured] = useState("");
 
+  const [message, setMessage] = useState(null);
 
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
 
-    const [message, setMessage] = useState(null);
+  const productDetails = useSelector((state) => state.productDetails);
+  const { loading, error, product } = productDetails;
 
-    const userLogin = useSelector(state => state.userLogin);
-    const { userInfo } = userLogin;
+  const productUpdate = useSelector((state) => state.productUpdate);
+  const {
+    loading: loadingUpdate,
+    error: errorUpdate,
+    success: successUpdate,
+  } = productUpdate;
 
-    const productDetails = useSelector(state => state.productDetails);
-    const { loading, error, product } = productDetails;
-
-    const productUpdate = useSelector((state) => state.productUpdate);
-    const { loading: loadingUpdate, error: errorUpdate, success: successUpdate } = productUpdate;
-
-    useEffect(() => {
-        if (successUpdate) {
-            dispatch({ type: PRODUCT_UPDATE_RESET });
-            navigate('/admin/products/list');
-        } else {
-            if (!product.name || product._id !== productId) {
-                dispatch(listProductDetails(productId));
-            } else {
-                setName(product.name);
-                setPrice(product.price);
-                setImage(product.image);
-                setBrand(product.brand);
-                setDescription(product.description);
-                setCategory(product.category);
-                setCountInStock(product.countInStock);
-            }
-        }
-        setMessage(null);
-    }, [dispatch, product, productId, navigate, successUpdate]);
-    const submitHandler = (e) => {
-        e.preventDefault();
-        dispatch(updateProduct({
-            _id: productId,
-            name,
-            price,
-            image,
-            brand,
-            description,
-            category,
-            countInStock,
-            featured
-        }));
-    };
-
-    const uploadFileHandler = async (e) => {
-        const file = e.target.files[0];
-        const formData = new FormData();
-        formData.append('image', file);
-        setUploading(true);
-        try {
-            const config = {
-                headers: {
-                    'Content-Type': 'multipart/form-data',
-                    Authorization: `Bearer ${userInfo.token}`,
-
-                }
-            };
-            const { data } = await axios.post('/api/upload', formData, config);
-            setImage(data);
-            setUploading(false);
-        } catch (error) {
-            console.error(error);
-            setUploading(false);
-        }
-    };
-
-    return (
-        <>
-            <Link to='/admin/products/list' className='btn btn-light my-3' >
-                {t('editProductScreen.return')}
-            </Link>
-            <FormContainer>
-                <h1>
-                    {t('editProductScreen.editProduct')}
-                </h1>
-                {message && <Message variant='danger'> {message} </Message>}
-                {loadingUpdate && <Loader />}
-                {errorUpdate && <Message variant='danger'> {errorUpdate} </Message>}
-                { }
-                {error && error.split(',').map(errorMessage => <Message key={errorMessage} variant='danger'> {errorMessage.trim().replace('Path ', '')} </Message>)}
-                {loading ? <Loader /> :
-                    <Form onSubmit={submitHandler}>
-                        <Form.Group controlId='name' className='pt-2'>
-                            <Form.Label>{t('name')}</Form.Label>
-                            <Form.Control
-                                type='text'
-                                placeholder='Name'
-                                value={name}
-                                onChange={(e) => setName(e.target.value)}
-                            ></Form.Control>
-                        </Form.Group>
-
-                        <Form.Group controlId='price' className='pt-2'>
-                            <Form.Label>{t('price')}</Form.Label>
-                            <Form.Control
-                                type='number'
-                                placeholder='Price'
-                                value={price}
-                                onChange={(e) => setPrice(e.target.value)}
-                            ></Form.Control>
-                        </Form.Group>
-                        <Form.Group controlId='category' className='pt-2'>
-                            <Form.Label>{t('category')}</Form.Label>
-                            <Form.Control
-                                type='text'
-                                placeholder='Category'
-                                value={category}
-                                onChange={(e) => setCategory(e.target.value)}
-                            ></Form.Control>
-                        </Form.Group>
-                        <Form.Group controlId='description' className='pt-2'>
-                            <Form.Label>{t('description')}</Form.Label>
-                            <Form.Control
-                                type='text'
-                                placeholder='Description'
-                                value={description}
-                                onChange={(e) => setDescription(e.target.value)}
-                            ></Form.Control>
-                        </Form.Group>
-                        <Form.Group controlId='brand' className='pt-2'>
-                            <Form.Label>{t('brand')}</Form.Label>
-                            <Form.Control
-                                type='text'
-                                placeholder='Brand'
-                                value={brand}
-                                onChange={(e) => setBrand(e.target.value)}
-                            ></Form.Control>
-                        </Form.Group>
-                        <Form.Group controlId='image' className='pt-2'>
-                            <Form.Label>{t('image')}</Form.Label>
-                            <Form.Control
-                                type='string'
-                                placeholder='Image url'
-                                value={image}
-                                onChange={(e) => setImage(e.target.value)}
-                            ></Form.Control>
-                            <Form.Control
-                                type='file'
-                                label='Choose File'
-                                onChange={uploadFileHandler}
-                            >
-                            </Form.Control>
-                            {uploading && <Loader />}
-                        </Form.Group>
-                        <Form.Group controlId='countInStock' className='pt-2'>
-                            <Form.Label>{t('countInStock')}</Form.Label>
-                            <Form.Control
-                                type='number'
-                                placeholder='CountInStock'
-                                value={countInStock}
-                                onChange={(e) => setCountInStock(e.target.value)}
-                            ></Form.Control>
-                        </Form.Group>
-                        <Form.Group controlId='featured' className='pt-2'>
-                            <Form.Check
-                                type='checkbox'
-                                label='Featured'
-                                checked={featured}
-                                onChange={(e) => setFeatured(e.target.checked)}
-                            ></Form.Check>
-                        </Form.Group>
-                        <Button type='submit' variant='primary' className='text-center my-2'>
-                            {t('update')}
-                        </Button>
-                    </Form>
-                }
-
-            </FormContainer >
-        </>
+  useEffect(() => {
+    if (successUpdate) {
+      dispatch({ type: PRODUCT_UPDATE_RESET });
+      navigate("/admin/products/list");
+    } else {
+      if (!product.name || product._id !== productId) {
+        dispatch(listProductDetails(productId));
+      } else {
+        setName(product.name);
+        setPrice(product.price);
+        setImage(product.image);
+        setBrand(product.brand);
+        setDescription(product.description);
+        setCategory(product.category);
+        setCountInStock(product.countInStock);
+      }
+    }
+    setMessage(null);
+  }, [dispatch, product, productId, navigate, successUpdate]);
+  const submitHandler = (e) => {
+    e.preventDefault();
+    dispatch(
+      updateProduct({
+        _id: productId,
+        name,
+        price,
+        image,
+        brand,
+        description,
+        category,
+        countInStock,
+        featured,
+      })
     );
+  };
+
+  const uploadFileHandler = async (e) => {
+    const file = e.target.files[0];
+    const formData = new FormData();
+    formData.append("image", file);
+    setUploading(true);
+    try {
+      const config = {
+        headers: {
+          "Content-Type": "multipart/form-data",
+          Authorization: `Bearer ${userInfo.token}`,
+        },
+      };
+      const { data } = await axios.post("/api/upload", formData, config);
+      setImage(data.filename);
+      setUploading(false);
+    } catch (error) {
+      console.error(error);
+      setUploading(false);
+    }
+  };
+
+  return (
+    <>
+      <Link to="/admin/products/list" className="btn btn-light my-3">
+        {t("editProductScreen.return")}
+      </Link>
+      <FormContainer>
+        <h1>{t("editProductScreen.editProduct")}</h1>
+        {message && <Message variant="danger"> {message} </Message>}
+        {loadingUpdate && <Loader />}
+        {errorUpdate && <Message variant="danger"> {errorUpdate} </Message>}
+        {}
+        {error &&
+          error.split(",").map((errorMessage) => (
+            <Message key={errorMessage} variant="danger">
+              {" "}
+              {errorMessage.trim().replace("Path ", "")}{" "}
+            </Message>
+          ))}
+        {loading ? (
+          <Loader />
+        ) : (
+          <Form onSubmit={submitHandler}>
+            <Form.Group controlId="name" className="pt-2">
+              <Form.Label>{t("name")}</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+
+            <Form.Group controlId="price" className="pt-2">
+              <Form.Label>{t("price")}</Form.Label>
+              <Form.Control
+                type="number"
+                placeholder="Price"
+                value={price}
+                onChange={(e) => setPrice(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+            <Form.Group controlId="category" className="pt-2">
+              <Form.Label>{t("category")}</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Category"
+                value={category}
+                onChange={(e) => setCategory(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+            <Form.Group controlId="description" className="pt-2">
+              <Form.Label>{t("description")}</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Description"
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+            <Form.Group controlId="brand" className="pt-2">
+              <Form.Label>{t("brand")}</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Brand"
+                value={brand}
+                onChange={(e) => setBrand(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+            <Form.Group controlId="image" className="pt-2">
+              <Form.Label>{t("image")}</Form.Label>
+              <Form.Control
+                type="string"
+                placeholder="Image url"
+                disabled={true}
+                value={image}
+                onChange={(e) => setImage(e.target.value)}
+              ></Form.Control>
+              <Form.Control
+                type="file"
+                label="Choose File"
+                onChange={uploadFileHandler}
+              ></Form.Control>
+              {uploading && <Loader />}
+            </Form.Group>
+            <Form.Group controlId="countInStock" className="pt-2">
+              <Form.Label>{t("countInStock")}</Form.Label>
+              <Form.Control
+                type="number"
+                placeholder="CountInStock"
+                value={countInStock}
+                onChange={(e) => setCountInStock(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+            <Form.Group controlId="featured" className="pt-2">
+              <Form.Check
+                type="checkbox"
+                label="Featured"
+                checked={featured}
+                onChange={(e) => setFeatured(e.target.checked)}
+              ></Form.Check>
+            </Form.Group>
+            <Button
+              type="submit"
+              variant="primary"
+              className="text-center my-2"
+            >
+              {t("update")}
+            </Button>
+          </Form>
+        )}
+      </FormContainer>
+    </>
+  );
 };
 
 export default EditProductScreen;
